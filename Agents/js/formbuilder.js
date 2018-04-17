cf102c0026184581eb020649cc6c2df1f1708f0f
tommyc325 <tommyc325@gmail.com>
2018-04-16 22:58:44 -0400

    Garbage
    Garbage

--- Agents/js/formbuilder.js
+++ Agents/js/formbuilder.js
@@ -17,13 +17,13 @@ var form4info = [
 {
   "case":189,
   "f2value":"Above Daily Lead Quantity Limit",
-  "form_html":'<div class="alert alert-warning">Lead must be submitted within 1 (one) business hour of receipt for refund approval.</div>',
+  "formhtml":'<div class="alert alert-warning">Lead must be submitted within 1 (one) business hour of receipt for refund approval. Please answer the additional questions below.</div>',
   "info_needed":"No further information required"
 },
 {
   "case":191,
   "f2value":"All Phone Numbers Provided are Disconnected",
-  "form_html":'<div class="alert alert-warning">Lead must be submitted within 1 (one) business hour of receipt for refund approval.</div>',
+  "formhtml":'<div class="alert alert-warning">Lead must be submitted within 1 (one) business hour of receipt for refund approval.</div>',
   "info_needed":"No further information required"
 },
 {
@@ -41,7 +41,7 @@ var form4info = [
 {
   "case":197,
   "f2value":"Duplicate Lead Received from Someone Else (Not Quote Storm) Today",
-  "formhtml":'<div class="form-group"> <label for="provide">Please provide the name of the lead provider, date, and time of receipt.</label> <div class="form-group  comment-data"> <input type="text" class="form-control comment-data validate-req" id="leadProvider" placeholder="Name of the lead provider."> </div><div class="row"> <div class="col"> <input type="text" class="form-control" id="date" placeholder="Date - mm/dd/yy"> </div><div class="col"> <input type="text" class="form-control" id="date" placeholder="Time - hh:mm am/pm"> </div></div></div>',
+  "formhtml":'<div class="alert alert-warning">Lead must be submitted within 1 (one) business hour of receipt for refund approval. Please answer the additional questions below.</div><div class="form-group"> <label for="provide">Please provide the name of the lead provider, date, and time of receipt.</label> <div class="form-group  comment-data"> <input type="text" class="form-control comment-data validate-req" id="leadProvider" placeholder="Name of the lead provider."> </div><div class="row"> <div class="col"> <input type="text" class="form-control" id="date" placeholder="Date - mm/dd/yy"> </div><div class="col"> <input type="text" class="form-control" id="date" placeholder="Time - hh:mm am/pm"> </div></div></div>',
   "info_needed":"Please provide the name of the lead provider, date, and time of receipt"
 },
 {
@@ -53,25 +53,25 @@ var form4info = [
 {
   "case":201,
   "f2value":"Incentivized Lead (e.g. Expects a gift card for quoting)",
-  "formhtml":'<div class="form-group"> <label for="describeIncentive">Please provide a few words describing the incentivization</label> <textarea class="form-control  comment-data validate-req" id="describeIncentive" maxlength="500" rows="3"></textarea> </div>',
+  "formhtml":'<div class="alert alert-warning">Lead must be submitted within 24 (twenty-four) business hours for refund approval. Please answer the additional questions below.</div><div class="form-group"> <label for="describeIncentive">Please provide a few words describing the incentivization</label> <textarea class="form-control  comment-data validate-req" id="describeIncentive" maxlength="500" rows="3"></textarea> </div>',
   "info_needed":"Please provide a few words describing the incentivization"
 },
 {
   "case":203,
   "f2value":"Insured with the Captive Carrier who Appointed You (Captive Agents Onl",
-  "formhtml":'<div class="form-group"> <label for="leadId">Please provide the Captive Carrier and Agent (if known)</label> <textarea class="form-control  comment-data " id="describeIncentive" maxlength="500" rows="3"></textarea> </div>',
+  "formhtml":'<div class="alert alert-warning">Lead must be submitted within 24 (twenty-four) business hours for refund approval. Please answer the additional questions below.</div><div class="form-group"> <label for="leadId">Please provide the Captive Carrier and Agent (if known)</label> <textarea class="form-control  comment-data " id="describeIncentive" maxlength="500" rows="3"></textarea> </div>',
   "info_needed":"Please provide the Captive Carrier and Agent (if known)"
 },
 {
   "case":205,
   "f2value":"Lead Form Data Does Not Match Filter (This is the FORM itself not matching)",
-  "formhtml":'<div class="form-group"> <label for="leadId">Please provide the name of the field that does not match filter</label> <input type="text" class="form-control comment-data validate-req" id="noMatchFilter" placeholder=""> </div>',
+  "formhtml":'<div class="alert alert-warning">Lead must be submitted within 1 (one) business hour of receipt for refund approval. Please answer the additional questions below.</div><div class="form-group"> <label for="leadId">Please provide the name of the field that does not match filter</label> <input type="text" class="form-control comment-data validate-req" id="noMatchFilter" placeholder=""> </div>',
   "info_needed":"Please provide the name of the field that does not match filter"
 },
 {
   "case":207,
   "f2value":"Lead Form Data is Incorrect/Invalid (This is after you talk to the prospect)",
-  "formhtml":'<div class="form-group"> <label for="leadId">Please provide the field names that were incorrect and their correct values.</label> <textarea class="form-control  comment-data validate-req" id="correctValues" maxlength="500" rows="3"></textarea> </div>',
+  "formhtml":'<div class="alert alert-warning">Lead must be submitted within 1 (one) business hour of receipt for refund approval. Please answer the additional questions below.</div><div class="form-group"> <label for="leadId">Please provide the field names that were incorrect and their correct values.</label> <textarea class="form-control  comment-data validate-req" id="correctValues" maxlength="500" rows="3"></textarea> </div>',
   "info_needed":"Please provide the field names that were incorrect and their correct values"
 },
 {
